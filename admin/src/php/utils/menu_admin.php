01be05e51d3981ef1513198472b9b9c572ce79cf
LoicGerard01 <loic.gerard@condorcet.be>
2024-05-08 00:09:31 +0200

    maj css

--- admin/src/php/utils/menu_admin.php
+++ admin/src/php/utils/menu_admin.php
@@ -1,20 +1,27 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Nos commerces</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index_.php?page=accueil_admin.php">Accueil</a>
-                </li>
-            </ul>
-            <!--   <form class="d-flex" role="search">
-                   <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                   <button class="btn btn-outline-success" type="submit">Search</button>
-               </form>
-           </div>
-            -->
+<div class="barre-logo">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+
+            <a class="navbar-brand" href="#">
+                <img src="/admin/public/images/logo.jpg" alt="Logo" width="40" height="40">
+
+                <span class="titre"></span>LGTECH
+
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!--
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                -->
+            </div>
         </div>
-</nav>
+    </nav>
+</div>
